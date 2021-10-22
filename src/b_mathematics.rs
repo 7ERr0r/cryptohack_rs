b1b2a029277bc02dc2444903fd15d952501e070c
Szperak <Szperak@users.noreply.github.com>
2021-10-22 13:36:28 +0200

    refactor

--- src/b_mathematics.rs
+++ src/b_mathematics.rs
@@ -1,6 +1,8 @@
 use crate::big;
 use num_bigint::BigInt;
 
+mod tonelli_shanks;
+
 pub fn a_modular_math_quadratic_residues() {
     let p = 29;
     let ints = [14, 6, 11];
@@ -67,126 +69,13 @@ pub fn c_modular_math_modular_square_root() {
     let p = &big!(30531851861994333252675935111487950694414332763909083514133769861350960895076504687261369815735742549428789138300843082086550059082835141454526618160634109969195486322015775943030060449557090064811940139431735209185996454739163555910726493597222646855506445602953689527405362207926990442391705014604777038685880527537489845359101552442292804398472642356609304810680731556542002301547846635101455995732584071355903010856718680732337369128498655255277003643669031694516851390505923416710601212618443109844041514942401969629158975457079026906304328749039997262960301209158175920051890620947063936347307238412281568760161);
     
 
-    let root = my_tonelli_shanks(p, a);
+    
+    let root = tonelli_shanks::my_tonelli_shanks(p, a);
 
     println!("root flag: {}", root);
 }
 
-pub fn my_tonelli_shanks(p: &BigInt, n: &BigInt) -> BigInt {
-    use num_traits::One;
-    use num_traits::Zero;
 
-    let p_1 = p - 1;
-
-    let (q, s) = my_factor_powers_2(&p_1);
-
-    println!("q: {}", q);
-    println!("s: {}", s);
-
-    //let a1 =
-
-    let z = my_find_non_residue(p);
-    println!("z: {}", z);
-
-    let mut m = s.clone();
-    let mut c = z.modpow(&q, p);
-    let mut t = n.modpow(&q, p);
-    let mut r = n.modpow(&((&q + 1) / 2), p);
-
-    for iter in 0..1000 {
-        println!("--- iteration: {}", iter);
-        println!("t: {}", t);
-        println!("r: {}", r);
-        println!("m: {}", m);
-        if t.is_zero() {
-            return BigInt::zero();
-        } else if t.is_one() {
-            return r;
-        } else {
-            let i = my_find_t_1_squaring(&m, &t, p);
-            
-            let b = (&m - &i - 1) % p;
-            let b = (big!(2)).modpow(&b, p);
-            //println!("2**(m - i - 1) == {}", &b);
-            let b = &c.modpow(&b, p);
-            //println!("b <- {}", b);
-
-            m = i;
-            c = b.modpow(&big!(2), p);
-            //println!("c <- {}", c);
-            
-            t *= &c;
-            t %= p;
-
-            r *= b;
-            r %= p;
-        }
-    }
-    let root = BigInt::default();
-    root
-}
-
-// returns i, 0 < i < m, such that t**(2**i) == 1
-pub fn my_find_t_1_squaring(m: &BigInt, t: &BigInt, p: &BigInt) -> BigInt {
-    use num_traits::One;
-
-    println!("use repeated squaring to find the least i, 0 < i < M, such that t**(2**i) == 1");
-    println!("M: {}", m);
-
-    let mut i = BigInt::one();
-    while &i < m {
-        let squared = (&big!(2)).modpow(&i, p);
-        let squared = t.modpow(&squared, p);
-
-        if squared.is_one() {
-            println!("found i: {}", i);
-
-            break;
-        }
-
-        i += 1;
-    }
-
-    i
-}
-
-// returns (q, s)
-// where x = q * 2**s
-pub fn my_factor_powers_2(x: &BigInt) -> (BigInt, BigInt) {
-    use num_integer::Integer;
-    use num_traits::identities::Zero;
-
-    let mut q = x.clone();
-    let mut s = BigInt::zero();
-
-    while q.is_even() {
-        q = q / 2;
-
-        s += 1;
-    }
-    //let s = BigInt::default();
-
-    (q, s)
-}
-
-// return z, which is non-residue
-pub fn my_find_non_residue(p: &BigInt) -> BigInt {
-    let mut test: BigInt = big!(2);
-    let _minus1 = &(p - 1);
-    let legendre_exp = _minus1 / 2;
-
-    loop {
-        test += 1;
-
-        let legendre = test.modpow(&legendre_exp, p);
-
-        if &legendre == _minus1 {
-            break;
-        }
-    }
-
-    test
-}
 
 pub fn e_lattices_vectors() {
     use ultraviolet::vec::DVec3;
